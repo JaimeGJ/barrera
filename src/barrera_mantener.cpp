059468c49bbe0673a13dc63b10c4b395c20d2d53
Unknown <jaimegaji@gmail.com>
2018-05-05 20:04:53 +0200

    cambios menores

--- src/barrera_mantener.cpp
+++ src/barrera_mantener.cpp
@@ -10,42 +10,43 @@
 #define IS_OPEN 2    //BCM 27, pin 13
 #define IS_CLOSED 3  //BCM 22, pin 15
 
+std_msgs::Byte estado;
 void callback(const std_msgs::Int8::ConstPtr& msg)  //Barrier's open pin needs to be high until barrier is completely open
 {
-  std_msgs::Byte estado;
+  
   if (digitalRead(IS_CLOSED)){
 	if(msg->data == 1){
-		ROS_INFO("La barrera se está abriendo");
+		ROS_INFO("La barrera se esta abriendo");
 		while (digitalRead(IS_OPEN) == 0){
 			digitalWrite (PIN_ABRIR, HIGH);
 			digitalWrite (PIN_CERRAR, LOW);
  		}
- 		ROS_INFO("La barrera está abierta");
+ 		ROS_INFO("La barrera esta abierta");
  		estado.data=1;	
-		pub.publish(estado)
+		//pub.publish(estado);
   	}
   }
   if (digitalRead(IS_OPEN)){
 	if(msg->data == 2){
-		ROS_INFO("La barrera se está cerrando");
+		ROS_INFO("La barrera se esta cerrando");
 		while (digitalRead(IS_CLOSED) == 0){
 			digitalWrite (PIN_CERRAR, HIGH);
 			digitalWrite (PIN_ABRIR, LOW);
  		}
- 		ROS_INFO("La barrera está cerrada");
+ 		ROS_INFO("La barrera esta cerrada");
  		estado.data=0;	
-		pub.publish(estado)
+		//pub.publish(estado);
   	}
   }
   if (digitalRead(IS_OPEN) == 0 && digitalRead(IS_CLOSED) == 0){
-	ROS_INFO("La barrera se está cerrando por seguridad");
+	ROS_INFO("La barrera se esta cerrando por seguridad");
 	while (digitalRead(IS_CLOSED) == 0){
 	digitalWrite (PIN_CERRAR, HIGH);
 	digitalWrite (PIN_ABRIR, LOW);
 	}
 	ROS_INFO("La barrera se ha cerrado por seguridad");
 	estado.data=0;	
-	pub.publish(estado)
+	//pub.publish(estado);
   }
 }
 
@@ -64,6 +65,7 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("barrera_solicitud",10,callback);
     ros::Publisher pub = n.advertise<std_msgs::Byte>("/barrera_estado",10);
+    pub.publish(estado);
     ros::spin();
    	//return 0 ;
     
--- src/barrera_pulso.cpp
+++ src/barrera_pulso.cpp
@@ -10,38 +10,39 @@
 #define IS_OPEN 2    //BCM 27, pin 13
 #define IS_CLOSED 3  //BCM 22, pin 15
 
+std_msgs::Byte estado;
 void callback(const std_msgs::Int8::ConstPtr& msg)  //Barrier opens with a solicitude of a pulse
 {
-  std_msgs::Byte estado;
+
   if (digitalRead(IS_CLOSED)){
 	if(msg->data == 1){
-		ROS_INFO("La barrera se está abriendo");
+		ROS_INFO("La barrera se esta abriendo");
 		digitalWrite (PIN_ABRIR, HIGH);
 		digitalWrite (PIN_CERRAR, LOW);
 		while ( !digitalRead(IS_OPEN)){
 			digitalRead(IS_OPEN);
 		}		
-		ROS_INFO("La barrera está abierta");
+		ROS_INFO("La barrera esta abierta");
  		estado.data=1;	
-		pub.publish(estado)
+		//pub.publish(estado);
   	}
   }
   if (digitalRead(IS_OPEN)){
 	if(msg->data == 2){
-		ROS_INFO("La barrera se está cerrando");
+		ROS_INFO("La barrera se esta cerrando");
 		digitalWrite (PIN_CERRAR, HIGH);
 		digitalWrite (PIN_ABRIR, LOW);
 		while ( !digitalRead(IS_CLOSED)){
 			digitalRead(IS_CLOSED);
 		}		
-		ROS_INFO("La barrera está cerrada");
+		ROS_INFO("La barrera esta cerrada");
  		estado.data=0;	
-		pub.publish(estado)
+		//pub.publish(estado);
 
   	}
   }
   if (digitalRead(IS_OPEN) == 0 && digitalRead(IS_CLOSED) == 0){
-	ROS_INFO("La barrera se está cerrando por seguridad");
+	ROS_INFO("La barrera se esta cerrando por seguridad");
 	digitalWrite (PIN_CERRAR, HIGH);
 	digitalWrite (PIN_ABRIR, LOW);
 	while ( !digitalRead(IS_CLOSED)){
@@ -49,7 +50,7 @@ void callback(const std_msgs::Int8::ConstPtr& msg)  //Barrier opens with a solic
 	}
 	ROS_INFO("La barrera se ha cerrado por seguridad");
 	estado.data=0;	
-	pub.publish(estado)
+	//pub.publish(estado);
   }
 }
 
@@ -67,7 +68,7 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("barrera_solicitud",10,callback);
     ros::Publisher pub = n.advertise<std_msgs::Byte>("/barrera_estado",10);
-    
+    pub.publish(estado)
     ros::spin();
 
 }
